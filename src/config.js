fa5ac0d02e88362de87e2b823b4b33cecc9d49e8
floriesol <soler.florie@gmail.com>
2026-04-18 15:40:34 +0800

    lab9

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // ── Single place to update your IP ──────────────────────────────────────────
 // Run `ipconfig` to get your current IPv4 address and update here
-export const BASE_URL = 'http://192.168.254.116:8000/api';
+export const BASE_URL = 'http://192.168.1.246:8000/api';
 export const getToken = () => localStorage.getItem('adminToken');
